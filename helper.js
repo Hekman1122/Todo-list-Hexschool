7cb2d057ec894446c35a88458a745ade41834e21
Hekman <yayakinetic@gmail.com>
2024-04-07 11:11:17 +0800

    first commit

---
+++ helper.js
@@ -0,0 +1,19 @@
+function errorHandler(res) {
+  const headers = {
+    "Access-Control-Allow-Headers":
+      "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+    "Content-Type": "application/json",
+  };
+  res.writeHead(400, headers);
+  res.write(
+    JSON.stringify({
+      status: "Request failed.",
+      message: "Invalid input or something went wrong.",
+    })
+  );
+  res.end();
+}
+
+module.exports = errorHandler;
